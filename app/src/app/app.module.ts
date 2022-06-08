9016b0ef02597adb1ad38cadccc0818580debc99
JordanZ <jordan.zelayahs@gmail.com>
2022-06-07 20:16:57 -0600

    Added header
    Signed-off-by: JordanZ <jordan.zelayahs@gmail.com>

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
