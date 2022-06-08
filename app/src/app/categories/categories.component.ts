1aa98950354e15b670ba016e340b5e0474e14211
JordanZ <jordan.zelayahs@gmail.com>
2022-06-08 15:18:21 -0600

    Added focus effect
    Signed-off-by: JordanZ <jordan.zelayahs@gmail.com>

--- app/src/app/categories/categories.component.ts
+++ app/src/app/categories/categories.component.ts
@@ -10,12 +10,16 @@ export class CategoriesComponent implements OnInit {
   public show(op:boolean){
     if(op){
       // @ts-ignore
-      document.getElementById("Popup").setAttribute("style", "visibility: visible")
+      document.getElementById("Popup").setAttribute("style", "visibility: visible; z-index: 4")
       // @ts-ignore
       document.getElementById("details").setAttribute("style", "visibility: hidden")
+      // @ts-ignore
+      document.getElementById("fade").setAttribute("style", "visibility: visible; z-index: 3")
     }else{
       // @ts-ignore
       document.getElementById("Popup").setAttribute("style", "visibility: hidden")
+      // @ts-ignore
+      document.getElementById("fade").setAttribute("style", "visibility: hidden; z-index: 0")
     }
   }
 
