e4bb87c6e5a6a35b68420f0982a01549177f63c9
JordanZ <jordan.zelayahs@gmail.com>
2022-06-08 19:45:41 -0600

    Added cards with JSON info
    Signed-off-by: JordanZ <jordan.zelayahs@gmail.com>

---
+++ API/src/Controllers/songController.js
@@ -0,0 +1,8 @@
+const songs = [{
+    name:"I love it"
+}]
+const songCtrl = {}
+
+song.getSongs = (req, res)=>{
+    res.json()
+}
--- app/src/app/categories/categories.component.ts
+++ app/src/app/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import mode from '../JSON/albums.json';
 
 @Component({
   selector: 'app-categories',
@@ -7,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  public show(op:boolean){
+  public albums:any[] = []
+  public songs:any[] = []
+  public show(op:boolean, album:any){
     if(op){
+
+      for(var i = 0; i<album.songs.length;i++){
+          this.songs.push(album.songs[i])
+          console.log(this.songs[i])
+      }
+      // @ts-ignore
+      document.getElementById("title").innerText = album.name + " by " + album.author
+      // @ts-ignore
+      document.getElementById("about").innerText = album.about
+      // @ts-ignore
+      document.getElementById("cover").setAttribute("src", album.imageURL)
       // @ts-ignore
       document.getElementById("Popup").setAttribute("style", "visibility: visible; z-index: 4")
       // @ts-ignore
@@ -23,9 +37,18 @@ export class CategoriesComponent implements OnInit {
     }
   }
 
+  public add(){
+
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    for(let i = 0;i<mode.length;i++){
+      // @ts-ignore
+      this.albums.push(mode[i])
+      console.log(this.albums[i]);
+    }
   }
 
 }
